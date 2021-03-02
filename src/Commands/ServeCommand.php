bf0aead0c97f45be1a7f158b3060ddaa879f10d7
bangnokia <bangnokia@gmail.com>
2021-03-02 20:34:14 +0700

    compatible for lower php version

--- src/Commands/ServeCommand.php
+++ src/Commands/ServeCommand.php
@@ -22,7 +22,7 @@ class ServeCommand extends Command
         $artisanPath = base_path('artisan');
 
         $processes = [
-            $httpProcess = new Process([$phpBinaryPath, $artisanPath, 'serve:http', ...$this->serveOptions()]),
+            $httpProcess = new Process([$phpBinaryPath, $artisanPath, 'serve:http', $this->serveOptions()->implode(' ')]),
             $socketProcess = new Process([$phpBinaryPath, $artisanPath, 'serve:websockets']),
         ];
 
@@ -62,6 +62,6 @@ class ServeCommand extends Command
                 }
 
                 return "--{$key}={$value}";
-            })->values();
+            });
     }
 }
